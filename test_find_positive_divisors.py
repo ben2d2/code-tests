5a51e207e847e5d0055c67d2ea200db8ccac2fad
ben2d2 <ben2d2@gmail.com>
2018-11-26 21:08:35 -0600

    Test FindPositiveDivisors calculate function

--- test_find_positive_divisors.py
+++ test_find_positive_divisors.py
@@ -2,11 +2,21 @@ import unittest
 
 class FindPositiveDivisors():
     def calculate(self, value):
-        return []
+        divisors = [value]
+        for i in range(1, value):
+            if value % i == 0:
+                divisors.append(i)
+        return sorted(divisors)
 
 class TestFindPositiveDivisors(unittest.TestCase):
     def setUp(self):
         self._class = FindPositiveDivisors()
+
+    def test_calculate(self):
+        self.assertEqual(
+            self._class.calculate(60),
+            [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60]
+        )
 
 if __name__ == '__main__':
     unittest.main()
