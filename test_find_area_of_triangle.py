456c849b46108fb2a32a7eb509d863f17eedb3ed
ben2d2 <ben2d2@gmail.com>
2018-11-26 22:13:12 -0600

    Test FindAreaOfTriangle calculate function

--- test_find_area_of_triangle.py
+++ test_find_area_of_triangle.py
@@ -1,12 +1,25 @@
 import unittest
+import math
 
 class FindAreaOfTriangle():
-    def calculate(self, values):
-        pass
+    def calculate_height(self, base, values):
+        a, b = values
+        tmp_area = (float(a) / float(2)) * float(b)
+        return (tmp_area / float(base / 2))
+
+    def calculate(self, a, b, c):
+        sides = [a, b, c]
+        base = float(max(sides))
+        sides.remove(base)
+        height = self.calculate_height(base, sides)
+        return (height * base) / 2
 
 class TestFindAreaOfTriangle(unittest.TestCase):
     def setUp(self):
         self._class = FindAreaOfTriangle()
+
+    def test_calculate(self):
+        self.assertEqual(self._class.calculate(3, 4, 5), 6)
 
 if __name__ == '__main__':
     unittest.main()
