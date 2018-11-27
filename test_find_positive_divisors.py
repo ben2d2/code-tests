c6265547086ae3dcabab0529997049c12fa8913e
ben2d2 <ben2d2@gmail.com>
2018-11-26 20:54:12 -0600

    Set up FindPositiveDivisors test case

---
+++ test_find_positive_divisors.py
@@ -0,0 +1,12 @@
+import unittest
+
+class FindPositiveDivisors():
+    def calculate(self, value):
+        return []
+
+class TestFindPositiveDivisors(unittest.TestCase):
+    def setUp(self):
+        self._class = FindPositiveDivisors()
+
+if __name__ == '__main__':
+    unittest.main()
