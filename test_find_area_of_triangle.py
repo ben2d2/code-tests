e9602fe4486a526c49e6ba481b8f36e675b5e7ab
ben2d2 <ben2d2@gmail.com>
2018-11-26 21:22:39 -0600

    Set up FindAreaOfTriangle test case

---
+++ test_find_area_of_triangle.py
@@ -0,0 +1,12 @@
+import unittest
+
+class FindAreaOfTriangle():
+    def calculate(self, values):
+        pass
+
+class TestFindAreaOfTriangle(unittest.TestCase):
+    def setUp(self):
+        self._class = FindAreaOfTriangle()
+
+if __name__ == '__main__':
+    unittest.main()
