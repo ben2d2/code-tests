15b9e90451bb3fe87c7b45392bcdbbf93fac54d1
ben2d2 <ben2d2@gmail.com>
2018-11-26 21:12:06 -0600

    Additional assertions

--- test_find_positive_divisors.py
+++ test_find_positive_divisors.py
@@ -18,5 +18,21 @@ class TestFindPositiveDivisors(unittest.TestCase):
             [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60]
         )
 
+        self.assertEqual(
+            self._class.calculate(42),
+            [1, 2, 3, 6, 7, 14, 21, 42]
+        )
+
+        self.assertEqual(
+            self._class.calculate(50),
+            [1, 2, 5, 10, 25, 50]
+        )
+
+        self.assertEqual(
+            self._class.calculate(78),
+            [1, 2, 3, 6, 13, 26, 39, 78]
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
